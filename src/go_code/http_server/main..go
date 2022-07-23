0608ae55e081ced95dad784892dda252166065dd
comboon <46803133+comboon@users.noreply.github.com>
2022-07-23 17:35:05 +0800

    new

--- src/go_code/http_server/main..go
+++ src/go_code/http_server/main..go
@@ -10,39 +10,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting http server...")
-	http.HandleFunc("/test", rootHandler)
-	err := http.ListenAndServe(":80", nil)
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", rootHandler)
-	// mux.HandleFunc("/healthz", healthz)
-	// mux.HandleFunc("/debug/pprof/", pprof.Index)
-	// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	// err := http.ListenAndServe(":80", mux)
+	fmt.Println("Start server...")
+	http.HandleFunc("/test", healthz)
+	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
 
-// func healthz(w http.ResponseWriter, r *http.Request) {
-// 	io.WriteString(w, "ok\n")
-// }
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("entering root handler")
-	user := r.URL.Query().Get("user")
-	version :=os.Getenv("Version")
+func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("client request...")
+	name := r.URL.Query().Get("name")
+	version :=os.Getenv("version")
 	//fmt.Println(version)
 	io.WriteString(w, fmt.Sprintf("version=%s\n", version))
-	if user != "" {
-		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	if name != "" {
+		io.WriteString(w, fmt.Sprintf("hello %s\n", name))
 	} else {
-		io.WriteString(w, "hello [stranger]\n")
+		io.WriteString(w, "hello default\n")
 	}
-	io.WriteString(w, "===================Details of the http request header:============\n")
 	for k, v := range r.Header {
 		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
 	}
