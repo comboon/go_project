1145c0b1eaffab4395ce611290a0d41735e38048
Administrator <admin@example.com>
2022-07-26 22:43:43 +0800

    add

---
+++ src/go__sjdev/unit-2/test.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+	var s string
+	fmt.Println(s)
+	var i ,j, k int
+	var b ,f ,s =true, 3.2 ,"dada"
+	i ,j := 1, 2
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	f.Close
+}
